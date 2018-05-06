7e5b7da29d648604aa528394b7a493bd03a908c1
Vladyslav Lazariv <lazariv98@gmail.com>
2018-05-06 23:05:02 +0300

    Add project files.

---
+++ FirstTaskXamarin/FirstTaskXamarin/QuotesPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstTaskXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        int i = 0;
+        private string[] _quotes = new string[]
+        {
+            "Hryshchyshyn Krasava.",
+            "Dima Krasva.",
+            "Max Krasava."
+        };
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            currentQuoteLabel.Text = _quotes[i];
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            i++;
+            if (i >= _quotes.Length)
+            {
+                i = 0;
+            }
+
+            currentQuoteLabel.Text = _quotes[i];
+        }
+    }
+}
